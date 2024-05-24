1f3f0791cccff52148b2542cc5aa162fc7c9fc3b
scaleup-viv <viv@scaleup.agency>
2024-05-24 09:41:49 +0200

    Delete the first commit because I put the wrong root

--- www-scaleup-tools-rework-main/src/components/Navbar/navbarConsts.js
+++
@@ -1,8 +0,0 @@
-export const navigationLinks = [
-  { title: 'HOME', link: '/' },
-  { title: 'SCALEUP TOOLS', link: '/#in-house-tools' },
-  { title: '100+ OTHER', link: '/#external-tools' },
-  // { title: 'PRICING', link: '/#pricing' },
-  // { title: 'CAREER', link: '/career' },
-  { title: 'CONTACT', link: '/contact' },
-];
--- www-scaleup-tools-rework-main/src/pages/api/users/get.js
+++
@@ -1,9 +0,0 @@
-import prisma from "../../../../lib/prisma";
-
-const handler = async (req, res) => {
-  console.log("getting users in API");
-  const users = await prisma.users.findMany();
-  res.status(200).json(users);
-};
-
-export default handler;
