016e23f10596b9d0ab9ad3d24b412b7a45b9421e
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-06-04 10:50:15 +0200

    debugging a server error coming from getDescriptions endpoint

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,43 +1,43 @@
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import prisma from "../../../../lib/prisma";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "../../../../lib/prisma";
 
-// export default NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//   ],
-//   callbacks: {
-//     async signIn({ user, account, profile, email, credentials }) {
-//       const dbUser = await prisma.users.findFirst({
-//          where: {
-//            email: user.email,
-//          },
-//       });
-//       if (dbUser) {
-//         return dbUser;
-//       } else {
-//         return false;
-//       }
-//     },
-//     async jwt({ token, profile }) {
-//       if (profile && profile.email) {
-//        const dbUser = await prisma.users.findFirst({
-//          where: {
-//            email: profile.email,
-//          },
-//        });
-//        if (dbUser) {
-//          token.role = dbUser.role;
-//        }
-//       }
-//       return token;
-//     },
-//     async session({ session, token }) {
-//       session.user.role = token.role;
-//       return session;
-//     },
-//   },
-// });
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const dbUser = await prisma.users.findFirst({
+         where: {
+           email: user.email,
+         },
+      });
+      if (dbUser) {
+        return dbUser;
+      } else {
+        return false;
+      }
+    },
+    async jwt({ token, profile }) {
+      if (profile && profile.email) {
+       const dbUser = await prisma.users.findFirst({
+         where: {
+           email: profile.email,
+         },
+       });
+       if (dbUser) {
+         token.role = dbUser.role;
+       }
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.role = token.role;
+      return session;
+    },
+  },
+});
--- src/pages/api/getSheetsData/getDescriptions.js
+++ src/pages/api/getSheetsData/getDescriptions.js
@@ -1,36 +1,28 @@
 import { google } from 'googleapis';
 import path from 'path';
 
-const getToolDescriptions = async (req, res) => {
+const getSheetsData = async (req, res) => {
   try {
-    // Set up Google Sheets API
     const sheets = google.sheets('v4');
     const auth = new google.auth.GoogleAuth({
       keyFile: path.join(process.cwd(), 'lib/serviceaccount.json'), // Adjust the path as needed
       scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     });
+
     const authClient = await auth.getClient();
 
-    // Specify the spreadsheetId and range to fetch data from
-    const spreadsheetId = '19NSpjRMRGzJ-wWf1icdE4tl2-svXkFlbDr-2Fv4CpWk';
-    const range = 'Filtered!B:P'; // Adjust the range as per your sheet
-
-    // Fetch data from Google Sheets
     const response = await sheets.spreadsheets.values.get({
       auth: authClient,
-      spreadsheetId,
-      range,
+      spreadsheetId: '1WOJG9S4P2OGyzFIma_QVSEq7tEp0mc56KSlJfPnN0mg',
+      range: 'logos!A:C', // Adjust the range as per your sheet
     });
 
     const rows = response.data.values;
     if (rows.length) {
-      // Format the data to return Tool ID and Description columns only
       const formattedRows = rows.map((row) => ({
-        tool_id: row[0], // Assuming Tool ID is in the first column (B)
-        description: row[8], // Assuming Description is in the seventh column (H)
-        twitter: row[9],
-        facebook: row[10],
-        
+        id: row[0],
+        logoLink: row[1],
+        company: row[2],
       }));
       res.status(200).json(formattedRows);
     } else {
@@ -42,4 +34,4 @@ const getToolDescriptions = async (req, res) => {
   }
 };
 
-export default getToolDescriptions;
+export default getSheetsData;
