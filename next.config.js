580c9757696f234fb9fc142d6be8e5763a16798f
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-06-10 12:39:02 +0200

    increase server timeout time so the descriptions can load properly

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
 
 const nextConfig = {
   reactStrictMode: true,
-
+  serverRuntimeConfig: {
+    // Increase timeout to 1 minute (optional, adjust as needed)
+    apiTimeout: 60000, // in milliseconds
+  },
 }
 
 module.exports = nextConfig
