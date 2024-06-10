a340f75bb75d3f4474e6852e430b4bbd730664ac
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-06-11 00:09:21 +0200

    replace the wrong cloud function endpoint

--- src/pages/api/getAItools/tools.js
+++ src/pages/api/getAItools/tools.js
@@ -1,6 +1,6 @@
 
 export default async function getTools(req, res) {
-  const response = await fetch('https://databasewithdesc3-2ttopirmva-uc.a.run.app/', {
+  const response = await fetch('https://cloudfunctions-2ttopirmva-uc.a.run.app', {
         method: 'GET'})
         const data = await response.json();
        res.status(200).json(data);
